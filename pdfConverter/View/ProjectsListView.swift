9f22840a1949f933f5a83f5bbde209ba68713853
nyoki610 <yuya.nyoki@gmail.com>
2025-03-04 02:55:51 +0900

    commit test

--- pdfConverter/View/ProjectsListView.swift
+++ pdfConverter/View/ProjectsListView.swift
@@ -39,6 +39,8 @@ struct ProjectsListView: View {
                 
                 VStack {
                     
+                    Text("Git test")
+                    
                     Spacer()
                     HStack {
                         Spacer()
