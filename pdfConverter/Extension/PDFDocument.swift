0b3756785ee8f878521c105da7559c62e5f0e2dc
nyoki610 <yuya.nyoki@gmail.com>
2025-04-06 23:07:57 +0900

    並び替え機能のViewを作成（機能自体は未実装）

---
+++ pdfConverter/Extension/PDFDocument.swift
@@ -0,0 +1,8 @@
+//
+//  PDFDocument.swift
+//  pdfConverter
+//
+//  Created by 二木裕也 on 2025/03/12.
+//
+
+import Foundation
---
+++ pdfConverter/View/ShuffleView.swift
@@ -0,0 +1,18 @@
+//
+//  ShuffleView.swift
+//  pdfConverter
+//
+//  Created by 二木裕也 on 2025/03/13.
+//
+
+import SwiftUI
+
+struct ShuffleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShuffleView()
+}
