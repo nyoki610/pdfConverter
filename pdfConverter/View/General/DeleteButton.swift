dc1fea21a806eabaa6d6bfd4781f5cdd7974acce
nyoki610 <yuya.nyoki@gmail.com>
2025-04-11 15:25:30 +0900

    ・表紙ページを追加する機能を実装 ・ShareLinkの実装を修正

---
+++ pdfConverter/View/General/DeleteButton.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteButton.swift
+//  pdfConverter
+//
+//  Created by 二木裕也 on 2025/04/11.
+//
+
+import SwiftUI
+
+struct DeleteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeleteButton()
+}
