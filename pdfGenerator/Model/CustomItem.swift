3cbffafc3a87a716b14f93832b0a2fea7fa2c64e
nyoki610 <yuya.nyoki@gmail.com>
2025-04-23 13:42:54 +0900

    以下を修正 ・アプリアイコン&タイトルを編集 ・addContents()をユーザーの指定した準備で画像が追加されるように修正 ・Contentのプロパティ名を修正（issue -> title, repairContent -> detail）

---
+++ pdfGenerator/Model/CustomItem.swift
@@ -0,0 +1,45 @@
+//
+//  Circle.swift
+//  pdfGenerator
+//
+//  Created by 二木裕也 on 2025/04/07.
+//
+
+import SwiftUI
+import RealmSwift
+
+
+protocol CustomItem: View {
+    /// 中心からの位置を -1.0〜1.0 の割合で表示
+    var positionRate: CGSize { get set }
+
+    /// 大きさを直接指定
+    var size: CGFloat { get set }
+}
+
+
+struct CustomCircle: CustomItem {
+
+    var positionRate: CGSize = CGSize(width: 0.0, height: 0.0)
+    var size: CGFloat = DeviceType.model == .iPhone ? 50 : 100
+    
+    private let circleWidth: CGFloat = 4
+    
+    var body: some View {
+        Circle()
+            .stroke(Color.red, lineWidth: circleWidth)
+            .frame(width: size-circleWidth, height: size-circleWidth)
+    }
+}
+
+struct CustomArrow: CustomItem {
+    
+    var positionRate: CGSize = CGSize(width: 0.0, height: 0.0)
+    var size: CGFloat = DeviceType.model == .iPhone ? 50 : 100
+    
+    var body: some View {
+        Image(systemName: "line.diagonal.arrow")
+            .font(.system(size: size))
+            .foregroundColor(.red)
+    }
+}
---
+++ pdfGenerator/View/LogoView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoView.swift
+//  pdfGenerator
+//
+//  Created by 二木裕也 on 2025/04/19.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoView()
+}
